bce23668f150fed1288c52ef146d0ae6aa045d1f
sheepfleece <rowanmorrison@protonmail.ch>
2021-09-03 11:51:21 +0300

    Initial commit

---
+++ MyContract.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
+
+
+import "ERC20/IERC20.sol";
+import "ERC20/SafeERC20.sol";
+
+contract StakingPool {
+    using SafeMath for uint64;
+    using SafeERC20 for IERC20;
+
+    IERC20 private _stakingToken;
+
+    constructor(
+        IERC20 stakingToken_
+    ) public {
+        _stakingToken = stakingToken_;
+    }
+
+    function claim(uint64 amount)
+        external
+    {
+        address caller = msg.sender;
+        _stakingToken.safeTransfer(caller, amount);
+    }
+
+}
