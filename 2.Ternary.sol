8929c2f01d9eada0fcbe6c437e53467425c75bf0
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2021-09-27 01:12:49 +0300

    test

--- 2.Ternary.sol
+++ 2.Ternary.sol
@@ -1,10 +1,9 @@
 contract c {
-  function fst(uint32 a, uint32 b) public pure returns (uint32, uint32) {
+  function f(uint32 a, uint32 b) public pure returns (uint32, uint32) {
     return (a, b);
   }
 
-  function failure() public pure returns (uint256, uint32) {
-    uint256 b = 0;
-    return fst(0, 0);
+  function failure() public pure returns (uint32, uint32) {
+    return f(0, 0);
   }
 }
